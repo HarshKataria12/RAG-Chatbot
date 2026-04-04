bfae6a09e9de039b4e2fae2e30a30cca1f1d4f91
HarshKataria12 <132175053+HarshKataria12@users.noreply.github.com>
2026-04-04 23:10:42 +0200

    first commit

---
+++ api_groq.py
@@ -0,0 +1,21 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of France?"},
+        {"role": "user", "content": "What is the largest mammal?"}
+
+    ],    
+    temperature=1,
+    max_completion_tokens=1024,
+    top_p=1,
+    stream=True,
+    stop=None
+)
+for chunk in response:
+    print(chunk.choices[0].delta.content or "", end="")
